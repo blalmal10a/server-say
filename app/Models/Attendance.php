7d1a7c83c927799c75148b7d0d9701d8f393c1b0
ys-grouse <malsawma7777@gmail.com>
2024-01-07 14:05:06 +0530

    attendance fillable

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
-
+    protected $guarded = ['id'];
     public function users()
     {
         return $this->belongsToMany(User::class);
