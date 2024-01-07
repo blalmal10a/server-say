06b99066dbda5f3afaf265206dee8fcee272ed5b
ys-grouse <malsawma7777@gmail.com>
2024-01-07 22:16:24 +0530

    chore[add]: faith promise init

---
+++ app/Http/Controllers/FaithPromiseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFaithPromiseRequest;
+use App\Http\Requests\UpdateFaithPromiseRequest;
+use App\Models\FaithPromise;
+
+class FaithPromiseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFaithPromiseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FaithPromise $faithPromise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FaithPromise $faithPromise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFaithPromiseRequest $request, FaithPromise $faithPromise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FaithPromise $faithPromise)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_01_07_221329_create_faith_promise_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\FaithPromise;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faith_promise_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(FaithPromise::class);
+            $table->foreignIdFor(User::class);
+            $table->decimal('amount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faith_promise_user');
+    }
+};
