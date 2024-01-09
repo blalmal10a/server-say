a523c2daf4ea1f725e29487ceba7b2f2a48141c8
ys-grouse <malsawma7777@gmail.com>
2024-01-10 01:37:07 +0530

    feat[add]:method to fetch users from designation

--- app/Models/Designation.php
+++ app/Models/Designation.php
@@ -10,4 +10,9 @@ class Designation extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
